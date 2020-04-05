65d22707bb1e9c65e0d65818c8c4b5a5c2093985
milekag01 <milekag01@gmail.com>
2020-04-05 07:16:19 +0530

    generating authentication tokens from jwt

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -44,7 +45,13 @@ const userSchema = mongoose.Schema({
     },
     links: {
         type: Array
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 // middleware to hash the password before saving
@@ -58,7 +65,20 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
+// this kind of methods are instance specific.
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString() }, '13reasonswhy', {expiresIn: '10h'});
+    
+    // saving tokens to keep track of all login instances
+    user.tokens = user.tokens.concat({token: token});
+    await user.save();
+
+    return token;
+}
+
 // custom method to create a login route
+// static methods are accessible on models
 userSchema.statics.findByCredentials = async (email,password) => {
     const user = await User.findOne({email,email});
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,7 +6,9 @@ const router = new express.Router();
 router.post('/users/login', async (req,res) => {
     try {
         const user = await User.findByCredentials(req.body.email,req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({user,token});
+
     } catch(error) {
         res.status(400).send(error);
     }
@@ -18,7 +20,10 @@ router.post('/users', async (req,res) => {
 
     try {
         await user.save();
-        res.status(201).send(user)
+        // generating 1st token right after signup
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
+
     } catch(error) {
         res.status(400).send(error);    // client error
     }
