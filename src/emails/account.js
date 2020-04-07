2098ea8827d80e364f10fe77fe8f068ca6e63bf6
Milek Agrawal <milekag01@gmail.com>
2020-04-07 11:01:23 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,5 +1,5 @@
 // const sgMail = require('@sendgrid/mail');
-// const sendgridAPIKey = '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';
+// const sendgridAPIKey = '';
 
 // sgMail.setApiKey(sendgridAPIKey);
 // const msg = {
